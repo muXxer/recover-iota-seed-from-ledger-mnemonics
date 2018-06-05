455ef7b2ab2deb2054aba0828e0af51b0c42b7eb
muXxer <mux3r@web.de>
2018-06-05 21:17:56 +0200

    Add retry if user made a mistake

--- mnemonics_to_iota_seed.py
+++ mnemonics_to_iota_seed.py
@@ -20,56 +20,73 @@ def MnemonicsToIotaSeed(recovery_words,
     """
     master_seed     = mnemonic.Mnemonic.to_seed(mnemonic=' '.join(recovery_words), passphrase=passphrase)
     bip32_root_key  = bip32utils.BIP32Key.fromEntropy(master_seed)
-    
+
     bip44_purpose_key       = bip32_root_key.ChildKey(0x8000002C)               # Purpose
     bip44_coin_type_key     = bip44_purpose_key.ChildKey(0x8000107A)            # CoinType
     bip44_account_key       = bip44_coin_type_key.ChildKey(bip44_account)       # Account
     bip44_change_key        = bip44_account_key.ChildKey(bip44_change)          # Change
     bip44_address_index_key = bip44_change_key.ChildKey(bip44_address_index)    # Address index
-    
+
     if (sys.version_info.major >= 3):
         priv_key    = bytearray(bip44_address_index_key.PrivateKey())
         chain_code  = bytearray(bip44_address_index_key.C)
     else:
         priv_key    = bytearray.fromhex(bip44_address_index_key.PrivateKey().encode('hex'))
         chain_code  = bytearray.fromhex(bip44_address_index_key.C.encode('hex'))
-    
+
     trits_out = []
 
     kerl = Kerl()
     kerl.k.update(priv_key[0:32] + chain_code[0:16] + priv_key[16:32] + chain_code[0:32])
     kerl.squeeze(trits_out)
-    
+
     iota_seed = conv.trits_to_trytes(trits_out)
-    
+
     print("Seed: %s, Length: %d" % (iota_seed, len(iota_seed)))
 
 #===============================================================================
 def InputRecoveryWords():
     print("\nWelcome to IOTA seed recovery!")
     print("\nPlease enter your Ledger Nano S recovery phrase now:")
-    
+
     m = mnemonic.Mnemonic(language='english')
-    
+
     recovery_words = []
+    quit_loop = False
     for i in range(24):
-        word = input("   word #%d: " % (i+1))
-    
-        if word == '':
-            raise Exception("ERROR: word #%d was empty!" % (i+1))
-    
+        if quit_loop:
+            break
+
+        while True:
+            try:
+                word = input("   word #%d: " % (i+1))
+            except KeyboardInterrupt:
+                quit_loop = True
+                break
+
+            if word == '':
+                print("\nERROR: word #%d was empty!" % (i+1))
+                continue
+
+            try:
+                m.wordlist.index(word)
+            except ValueError:
+                print("\nERROR: word '%s' is not in the BIP44 words list!" % (word))
+                continue
+
+            recovery_words.append(word)
+            break
+
+    if not quit_loop:
+        print("\nPlease enter your Ledger Nano S passphrase now (only if set in the ledger, not your pin number!):")
         try:
-            m.wordlist.index(word)
-        except ValueError:
-            raise Exception("ERROR: word '%s' is not in the BIP44 words list!" % (word))
-        
-        recovery_words.append(word)
-    
-    print("\nPlease enter your Ledger Nano S passphrase now (only if set in the ledger, not your pin number!):")
-    passphrase = input("   Ledger passphrase: ")
-    print("\nCalculating your IOTA seed...")
-    
-    MnemonicsToIotaSeed(recovery_words, passphrase)
+            passphrase = input("   Ledger passphrase: ")
+        except KeyboardInterrupt:
+            quit_loop = True
+
+    if not quit_loop:
+        print("\nCalculating your IOTA seed...")
+        MnemonicsToIotaSeed(recovery_words, passphrase)
 
 #===============================================================================
 if __name__ == '__main__':
