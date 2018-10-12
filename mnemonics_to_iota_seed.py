f504b2b055d23cdcd318316a6116b9e2f937e013
muXxer <mux3r@web.de>
2018-10-12 10:00:41 +0200

    Changed BIP Path to latest IOTA Ledger App version

--- mnemonics_to_iota_seed.py
+++ mnemonics_to_iota_seed.py
@@ -6,33 +6,32 @@ from iota.crypto.kerl import Kerl, conv
 #===============================================================================
 def MnemonicsToIotaSeed(recovery_words, 
                         passphrase='', 
-                        bip44_account=0x80000000, 
-                        bip44_change=0x00000000, 
-                        bip44_address_index=0x00000000):
+                        bip44_account=0x00000000, 
+                        bip44_page_index=0x00000000):
     """Recover an IOTA seed from the ledger Nano S recovery phrase
 
     Keyword arguments:
     recovery_words -- a list of 24 words (your ledger recovery phrase)
     passphrase -- a string containing the passphrase (only if set in ledger, not your pin number!)
     bip44_account -- an integer containing BIP44 path 'Account'
-    bip44_change -- an integer containing BIP44 path 'Change'
-    bip44_address_index -- an integer containing BIP44 path 'Address index'
+    bip44_page_index -- an integer containing BIP44 path 'Page index'
     """
+    print("\nCalculating your IOTA seed...")
+    
     master_seed     = mnemonic.Mnemonic.to_seed(mnemonic=' '.join(recovery_words), passphrase=passphrase)
     bip32_root_key  = bip32utils.BIP32Key.fromEntropy(master_seed)
 
-    bip44_purpose_key       = bip32_root_key.ChildKey(0x8000002C)               # Purpose
-    bip44_coin_type_key     = bip44_purpose_key.ChildKey(0x8000107A)            # CoinType
-    bip44_account_key       = bip44_coin_type_key.ChildKey(bip44_account)       # Account
-    bip44_change_key        = bip44_account_key.ChildKey(bip44_change)          # Change
-    bip44_address_index_key = bip44_change_key.ChildKey(bip44_address_index)    # Address index
+    bip44_purpose_key       = bip32_root_key.ChildKey(0x8000002C)                       # Purpose
+    bip44_coin_type_key     = bip44_purpose_key.ChildKey(0x8000107A)                    # CoinType
+    bip44_account_key       = bip44_coin_type_key.ChildKey(0x80000000+bip44_account)    # Account
+    bip44_page_index_key    = bip44_account_key.ChildKey(0x80000000+bip44_page_index)   # Page index
 
     if (sys.version_info.major >= 3):
-        priv_key    = bytearray(bip44_address_index_key.PrivateKey())
-        chain_code  = bytearray(bip44_address_index_key.C)
+        priv_key    = bytearray(bip44_page_index_key.PrivateKey())
+        chain_code  = bytearray(bip44_page_index_key.C)
     else:
-        priv_key    = bytearray.fromhex(bip44_address_index_key.PrivateKey().encode('hex'))
-        chain_code  = bytearray.fromhex(bip44_address_index_key.C.encode('hex'))
+        priv_key    = bytearray.fromhex(bip44_page_index_key.PrivateKey().encode('hex'))
+        chain_code  = bytearray.fromhex(bip44_page_index_key.C.encode('hex'))
 
     trits_out = []
 
@@ -46,24 +45,19 @@ def MnemonicsToIotaSeed(recovery_words,
 
 #===============================================================================
 def InputRecoveryWords():
-    print("\nWelcome to IOTA seed recovery!")
     print("\nPlease enter your Ledger Nano S recovery phrase now:")
 
     m = mnemonic.Mnemonic(language='english')
 
     recovery_words = []
-    quit_loop = False
+    
     for i in range(24):
-        if quit_loop:
-            break
-
         while True:
             try:
                 word = input("   word #%d: " % (i+1))
             except KeyboardInterrupt:
-                quit_loop = True
-                break
-
+                return None
+            
             if word == '':
                 print("\nERROR: word #%d was empty!" % (i+1))
                 continue
@@ -76,18 +70,32 @@ def InputRecoveryWords():
 
             recovery_words.append(word)
             break
+    
+    return recovery_words
 
-    if not quit_loop:
-        print("\nPlease enter your Ledger Nano S passphrase now (only if set in the ledger, not your pin number!):")
-        try:
-            passphrase = input("   Ledger passphrase: ")
-        except KeyboardInterrupt:
-            quit_loop = True
+#===============================================================================
+def InputPassphrase():
+    print("\nPlease enter your Ledger Nano S passphrase now (only if set in the ledger, not your pin number!):")
+    
+    try:
+        return input("   Ledger passphrase: ")
+    except KeyboardInterrupt:
+        return None
 
-    if not quit_loop:
-        print("\nCalculating your IOTA seed...")
-        MnemonicsToIotaSeed(recovery_words, passphrase)
+#===============================================================================
+def RecoverSeed():
+    print("\nWelcome to IOTA Ledger Nano seed recovery!")
+    
+    recovery_words = InputRecoveryWords()
+    if recovery_words == None:
+        return
+    
+    passphrase = InputPassphrase()
+    if passphrase == None:
+        return
+    
+    MnemonicsToIotaSeed(recovery_words, passphrase)
 
 #===============================================================================
 if __name__ == '__main__':
-    InputRecoveryWords()
+    RecoverSeed()
---
+++ mnemonics_to_iota_seed_romeo_wallet.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import xxhash
+from six.moves import input
+from mnemonics_to_iota_seed import MnemonicsToIotaSeed, InputRecoveryWords, InputPassphrase
+
+#===============================================================================
+def InputRomeoAccount():
+    print("\nPlease enter your Romeo account now (number ranging from 0 to 999999999, or an arbitrary text):")
+
+    try:
+        romeo_account = input("   Romeo account: ")
+        try:
+            account = int(romeo_account)
+        except:
+            # Account is not an integer, hash the text with xxhash
+            x = xxhash.xxh32(seed=0xABCD)
+            x.update(bytes(romeo_account.upper().lstrip().rstrip()))
+            account = x.intdigest()
+        
+        if account >= 1000000000:           # from 0 to 999999999
+            account_str = str(account)
+            account = int(account_str[len(account_str)-9:])
+        
+        return account       
+    except KeyboardInterrupt:
+        return None
+
+#===============================================================================
+def InputRomeoPageIndex():
+    print("\nPlease enter your Romeo page index now (it starts with index 1):")
+
+    try:
+        romeo_page = input("   Romeo page index: ")
+        page_index = int(romeo_page) - 1
+        if page_index < 0:
+            raise Exception("Invalid page index!")
+        return page_index
+    except KeyboardInterrupt:
+        return None
+
+#===============================================================================
+def RecoverRomeoSeed():
+    print("\nWelcome to IOTA Ledger Nano seed recovery for Romeo wallet!")
+    
+    recovery_words = InputRecoveryWords()
+    if recovery_words == None:
+        return
+    
+    passphrase = InputPassphrase()
+    if passphrase == None:
+        return
+    
+    account    = InputRomeoAccount()
+    if account == None:
+        return
+    
+    page_index = InputRomeoPageIndex()
+    if page_index == None:
+        return
+    
+    MnemonicsToIotaSeed(recovery_words, passphrase, bip44_account=account, bip44_page_index=page_index)
+
+#===============================================================================
+if __name__ == '__main__':
+    RecoverRomeoSeed()
